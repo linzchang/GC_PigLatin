15784c29ee2106e1d494f4a80876c3e030fcf5da
Lin-z Chang <lin-zchang@amrock.com>
2018-10-12 21:03:26 -0400

    Created a translate method and also added in some input validation

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 namespace PigLatin
 {
     
-
     class Program
     {
         static void Main(string[] args)
@@ -23,25 +22,20 @@ namespace PigLatin
                 //Prompt user for text to translate
                 Console.WriteLine("Enter a line of text to be translated: ");
                 userText = Console.ReadLine().ToLower();
+
+                //validate that user has entered a word
+                if (userText == "")
+                {
+                    Console.WriteLine("That is not valid input.");
+                    continue;
+                }
+                //ALSO can check for numbers and symbolds here
+
+                //break up User's input into an array of strings
                 phrase = userText.Split(' ');
 
-                foreach (var word in phrase)
-                {
-                    //check if word has no vowels
-                    if (!Regex.IsMatch(word, @"\w[aeiouAEIOU]"))
-                    {
-                        CheckForNoVowels(word);
-                    }
-                    //check if it starts with a vowel
-                    else if (Regex.IsMatch(word, @"\b[aeiouAEIOU]\w*"))
-                    {
-                        CheckVowel(word);
-                    }
-                    else
-                    {
-                        CheckConsonant(word);
-                    }  
-                }
+                //translate each word in array to Pig Latin and print to console
+                Translate(phrase);
 
                 Console.WriteLine(" ");
 
@@ -52,26 +46,29 @@ namespace PigLatin
                 {
                     break;
                 }
-            } 
-        }
-
-        public static void CheckVowel(string word)
-        {
-            //add -ay to the end of the word
-            Console.Write(word + "way" + " ");
-        }
-
-        public static void CheckConsonant(string word)
-        {
-            
-            while (Regex.IsMatch(word, @"\b[^aeiouAEIOU]\w*"))
-            {
-                //move all consonants before first vowel to the end of the word and add -ay
-                string firstLetter = word.Substring(0, 1);
-                string restOfWord = word.Substring(1, word.Length - 1);
-                word = restOfWord + firstLetter;
             }
-            Console.Write(word + "ay" + " ");
+        }
+
+        public static void Translate(string[] phrase)
+        {
+            foreach (var word in phrase)
+            {
+                //check if word has no vowels
+                if (!Regex.IsMatch(word, @"\w[aeiouAEIOU]"))
+                {
+                    CheckForNoVowels(word);
+                }
+                //check if it starts with a vowel
+                else if (Regex.IsMatch(word, @"\b[aeiouAEIOU]\w*"))
+                {
+                    CheckVowel(word);
+                }
+                //word must start with a consonant
+                else 
+                {
+                    CheckConsonant(word);
+                }
+            }
         }
 
         public static void CheckForNoVowels(string word)
@@ -79,5 +76,23 @@ namespace PigLatin
             //word doesn't contain any vowels, so add -way to end of the word
             Console.Write(word + "way" + " ");
         }
+
+        public static void CheckVowel(string word)
+        {
+            //word starts with a vowel, add -way to the end of the word
+            Console.Write(word + "way" + " ");
+        }
+
+        public static void CheckConsonant(string word)
+        {
+            while (Regex.IsMatch(word, @"\b[^aeiouAEIOU]\w*"))
+            {
+                //word starts with a consonant, move all consonants before first vowel to the end of the word and add -ay
+                string firstLetter = word.Substring(0, 1);
+                string restOfWord = word.Substring(1, word.Length - 1);
+                word = restOfWord + firstLetter;
+            }
+            Console.Write(word + "ay" + " ");
+        }  
     }
 }
